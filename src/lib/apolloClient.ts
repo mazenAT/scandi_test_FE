2d5bc5ba149c8bf645b8f92216709d1ff0e98ee6
mazenAT <mazenatef508@gmail.com>
2025-07-20 14:33:05 +0300

    feat: add Vercel proxy for GraphQL to fix mixed content and CORS
    - Add rewrite rule in vercel.json to proxy /api/graphql to InfinityFree backend
    - Update Apollo Client URI to use /api/graphql for secure frontend-backend communication
    - Fixes mixed content and CORS issues on Vercel deployment

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://jycwdzlv.infinityfree.com/graphql',
+  uri: '/api/graphql',
   cache: new InMemoryCache(),
 });
 
