f489d59b1976b65340495b21f7c35235d6d3f17e
mazenAT <mazenatef508@gmail.com>
2025-07-23 13:02:28 +0300

    using proxy to overcome issue with infinityFree hosting

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,7 +3,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const isLocal = typeof window !== 'undefined' && window.location.hostname === 'localhost';
 
 const client = new ApolloClient({
-  uri: isLocal ? 'http://localhost:8000/graphql' : 'https://scanditest.fwh.is/graphql',
+  uri: isLocal
+    ? 'http://localhost:8000/graphql'
+    : 'https://cors-anywhere.herokuapp.com/https://scanditest.fwh.is/graphql',
   cache: new InMemoryCache(),
 });
 
